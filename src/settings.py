a7f15852e1c67b3d84e7176c3e3e0ceb814d5b3e
Pavel Popov <paavali.popov@gmail.com>
2024-10-11 13:41:50 -0400

    updated settings

--- src/settings.py
+++ src/settings.py
@@ -3,19 +3,20 @@
 import datetime
 import os
 import platform
+from path import Path
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
-ASSETS_ROOT = os.path.join(PROJECT_ROOT, "assets")
-WEIGHTS_ROOT = os.path.join(ASSETS_ROOT, "model_weights")
-LOGS_ROOT = os.path.join(ASSETS_ROOT, "logs")
+PROJECT_ROOT = Path(os.path.dirname(os.path.dirname(__file__)))
+ASSETS_ROOT = PROJECT_ROOT.joinpath("assets")
+WEIGHTS_ROOT = ASSETS_ROOT.joinpath("model_weights")
+LOGS_ROOT = ASSETS_ROOT.joinpath("logs")
 
 UTCNOW = datetime.datetime.now(datetime.timezone.utc).strftime("%y%m%d_%H%M%S")
 
 node = platform.node()
 if "arctrd" in node:
-    DATA_ROOT = "/data/users2/ppopov1/datasets"
+    DATA_ROOT = Path("/data/users2/ppopov1/datasets")
 else:
-    DATA_ROOT = os.path.join(ASSETS_ROOT, "data")
+    DATA_ROOT = ASSETS_ROOT.joinpath("data")
 
 if __name__ == "__main__":
     print("PROJECT_ROOT: ", PROJECT_ROOT)
